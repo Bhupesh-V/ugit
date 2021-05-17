aeff6c5a48d52f2e995bedc9f741186743508ff0
Joe Block <jpb@unixorn.net>
2021-05-17 13:26:31 -0600

    Add oh-my-zsh-compatible plugin file.
    Add ugit.plugin.zsh. This is the standard name used by Oh-My-ZSH
    (and oh-my-zsh-compatible ZSH frameworks) for plugins, and once
    added to the user's configuration, will be loaded automatically
    during login, and add the repo directory to the user's `$PATH` to
    make `ugit` available.
    
    This is also used by smarter ZSH frameworks like Antigen and Zgenom
    to automatically keep the user's checkout of `ugit` up to date so
    they get automatic updates.
    
    Closes #10

---
+++ ugit.plugin.zsh
@@ -0,0 +1,7 @@
+# Copyright 2021 Joseph Block <jpb@unixorn.net>
+#
+# Licensed under the MIT license.
+
+# Append the repo's main directory to $PATH to make ugit available
+PLUGIN_BIN="$(dirname $0)"
+export PATH=${PATH}:${PLUGIN_BIN}
